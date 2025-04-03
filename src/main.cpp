30c6b58a4b884a39acac374d18eca01afa1cb962
Saranthyr <kirillmaksimets@gmail.com>
2025-04-03 16:58:32 +0300

    [+]imgui added

--- src/main.cpp
+++ src/main.cpp
@@ -13,26 +13,26 @@
 
 
 int main (int argc, char *argv[]) {
-    auto fullPath = std::filesystem::current_path() / "DVDLogo.png";
+    // auto fullPath = "assets/DVDLogo.png";
     sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Window", (sf::Style::Titlebar, sf::Style::Close));
     ImGui::SFML::Init(window);
     window.setPosition({0,0});
     int WINDOW_W = window.getSize().x;
     int WINDOW_H = window.getSize().y;
-    sf::Texture logoTex;
-    if (!logoTex.loadFromFile(fullPath)) {
-        std::cerr << "Failed to load texture from: " << fullPath << std::endl;
-        return 1;  // Exit if texture fails to load
-    }
-    logoTex.setSmooth(true);
+    // sf::Texture logoTex;
+    // if (!logoTex.loadFromFile(fullPath)) {
+    //     std::cerr << "Failed to load texture from: " << fullPath << std::endl;
+    //     return 1;  // Exit if texture fails to load
+    // }
+    // logoTex.setSmooth(true);
     float speed = 0.5f;
-    sf::Sprite spriteLogo(logoTex);
-    spriteLogo.setScale({0.25f, 0.25f});
-    spriteLogo.setPosition({
-        (static_cast<float>(WINDOW_W)/2) - spriteLogo.getGlobalBounds().size.x/2,
-        (static_cast<float>(WINDOW_H)/2) - spriteLogo.getGlobalBounds().size.y/2
-    });
-    sf::Vector2f velocity({speed, speed});
+    // sf::Sprite spriteLogo(logoTex);
+    // spriteLogo.setScale({0.25f, 0.25f});
+    // spriteLogo.setPosition({
+    //     (static_cast<float>(WINDOW_W)/2) - spriteLogo.getGlobalBounds().size.x/2,
+    //     (static_cast<float>(WINDOW_H)/2) - spriteLogo.getGlobalBounds().size.y/2
+    // });
+    // sf::Vector2f velocity({speed, speed});
     bool pause = false;
     sf::Clock deltaClock;
     while (window.isOpen())
@@ -68,32 +68,32 @@ int main (int argc, char *argv[]) {
         ImGui::Begin("Hello, world!");
         ImGui::Button("Look at this pretty button");
         ImGui::End();
-                spriteLogo.move(velocity);
-                sf::Vector2f boundsPos = spriteLogo.getGlobalBounds().position;
-                sf::Vector2f boundsSize = spriteLogo.getGlobalBounds().size;
-                if (boundsPos.x < 0 && velocity.x < 0) {
-                    velocity.x *= -1.0f;
-                }
-                if (boundsPos.y < 0 && velocity.y < 0) {
-                    velocity.y *= -1.0f;
-                }
+                // spriteLogo.move(velocity);
+                // sf::Vector2f boundsPos = spriteLogo.getGlobalBounds().position;
+                // sf::Vector2f boundsSize = spriteLogo.getGlobalBounds().size;
+                // if (boundsPos.x < 0 && velocity.x < 0) {
+                //     velocity.x *= -1.0f;
+                // }
+                // if (boundsPos.y < 0 && velocity.y < 0) {
+                //     velocity.y *= -1.0f;
+                // }
 
-                if (boundsPos.x + boundsSize.x > window.getSize().x && velocity.x > 0) {
-                    velocity.x *= -1.0f;
-                }
-                if (boundsPos.y + boundsSize.y > window.getSize().y && velocity.y > 0) {
-                    velocity.y *= -1.0f;
+                // if (boundsPos.x + boundsSize.x > window.getSize().x && velocity.x > 0) {
+                //     velocity.x *= -1.0f;
+                // }
+                // if (boundsPos.y + boundsSize.y > window.getSize().y && velocity.y > 0) {
+                //     velocity.y *= -1.0f;
                     
-                }
-                window.clear(sf::Color::Black);
-                window.draw(spriteLogo);
-                sf::RectangleShape boundsRect(spriteLogo.getGlobalBounds().size);
-                boundsRect.setPosition({spriteLogo.getGlobalBounds().position.x,
-                    spriteLogo.getGlobalBounds().position.y});
-                boundsRect.setOutlineColor(sf::Color::Red);
-                boundsRect.setOutlineThickness(5.0f);
-                boundsRect.setFillColor(sf::Color::Transparent);
-                window.draw(boundsRect);
+                // }
+                // window.clear(sf::Color::Black);
+                // window.draw(spriteLogo);
+                // sf::RectangleShape boundsRect(spriteLogo.getGlobalBounds().size);
+                // boundsRect.setPosition({spriteLogo.getGlobalBounds().position.x,
+                //     spriteLogo.getGlobalBounds().position.y});
+                // boundsRect.setOutlineColor(sf::Color::Red);
+                // boundsRect.setOutlineThickness(5.0f);
+                // boundsRect.setFillColor(sf::Color::Transparent);
+                // window.draw(boundsRect);
                 ImGui::SFML::Render(window);
             }
         window.display();
